aa685ca0e76f202887b49080441f22e5c3313e9e
나연 <kny5579@naver.com>
2025-04-30 16:21:37 +0900

    chore(#1): 재배포

--- src/main/java/com/couponmoa/backend/couponmoaai/CouponmoaAiApplication.java
+++ src/main/java/com/couponmoa/backend/couponmoaai/CouponmoaAiApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CouponmoaAiApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(CouponmoaAiApplication.class, args);
     }
