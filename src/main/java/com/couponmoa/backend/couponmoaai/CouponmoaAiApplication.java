7a830013fd8382ab8e1ea8678f65583785be4116
나연 <kny5579@naver.com>
2025-04-30 13:53:34 +0900

    init

---
+++ src/main/java/com/couponmoa/backend/couponmoaai/CouponmoaAiApplication.java
@@ -0,0 +1,13 @@
+package com.couponmoa.backend.couponmoaai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponmoaAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CouponmoaAiApplication.class, args);
+    }
+
+}
